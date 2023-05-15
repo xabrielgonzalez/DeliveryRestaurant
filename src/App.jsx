56eef0f56addc7702446053bfdff6b614d6238f0
xabrielgonzalez <xabriel241195@gmail.com>
2023-05-15 20:11:45 -0300

    boton testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
 /* 
 import {Homeindex} from './pages/Homeindex'
 import {HomeInicial} from './pages/HomeInicial'
 import {Restaurante} from './pages/Restaurante'
 import {Pasta} from './pages/Restaurante'
-
  */
-const Register =lazy(()=> import('./components/Register'));
 
+const Register =lazy(()=> import('./components/Register'));
 const Homeindex = lazy(() => import('./pages/Homeindex'));
 const HomeInicial =lazy(() =>import('./pages/HomeInicial'));
 const Restaurante = lazy(() => import('./pages/Restaurante'));
@@ -24,12 +25,10 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/restaurante" element={<Restaurante />} />
           <Route path="/home" element={<Homeindex />} />
-          <Route path="/pasta" element={<Pasta />} />
-      
+          <Route path="/pasta" element={<Pasta />}/>
         </Routes>
       </Suspense>
     </BrowserRouter>
   )
 }
-
 export default App;
--- src/svgComponents/Felicetail.jsx
+++ src/svgComponents/Felicetail.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+
 const Felicetail = (props) => (
   <svg
     width={492}

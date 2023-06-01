0aa80efd5a1c1aa72985368a523acd95d757428f
xabrielgonzalez <xabriel241195@gmail.com>
2023-06-01 19:06:00 -0300

    slider fix

--- src/components/ModalProductos.jsx
+++ src/components/ModalProductos.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Slider } from '@lifarl/react-scroll-snap-slider';
 import { Productos } from './Productos';
 
--- src/validation/valiForm.jsx
+++ src/validation/valiForm.jsx
@@ -1,63 +1,39 @@
-export const handleNameChange = (event, setInputValue) => {
+export const handleNameChange = (event) => {
   event.target.value = event.target.value.replace(/[^a-zA-Z]/g, '');
-  if (event.target.value.length <= 20) {
-    setInputValue(event.target.value);
-  }
 };
 
-export const handleEmailChange = (event, setInputValue) => {
+export const handleEmailChange = (event) => {
   event.target.value = event.target.value;
-  if (event.target.value.length <= 64) {
-    setInputValue(event.target.value);
-  }
+
 };
 
-export const handlePassChange = (event, setInputValue) => {
+export const handlePassChange = (event) => {
   event.target.value = event.target.value;
-  if (event.target.value.length <= 8) {
-    setInputValue(event.target.value);
-  }
 };
 
-export const handleRPassChange = (event, setInputValue) => {
+export const handleRPassChange = (event) => {
   event.target.value = event.target.value;
-  if (event.target.value.length <= 8) {
-    setInputValue(event.target.value);
-  }
+  
 };
 
-export const handleAddressChange = (event, setInputValue) => {
+export const handleAddressChange = (event) => {
   event.target.value = event.target.value;
-  if (event.target.value.length <= 20) {
-    setInputValue(event.target.value);
-  }
 };
 
-export const handleNumberChange = (event, setInputValue) => {
+export const handleNumberChange = (event) => {
   event.target.value = event.target.value.replace(/[^0-9]/g, '');
-  if (event.target.value.length <= 3) {
-    setInputValue(event.target.value);
-  }
 };
 
-export const handleFloorChange = (event, setInputValue) => {
+export const handleFloorChange = (event) => {
   event.target.value = event.target.value.replace(/[^0-9]/g, '');
-  if (event.target.value.length <= 2) {
-    setInputValue(event.target.value);
-  }
 };
 
-export const handleNumberCardChange = (event, setInputValue) => {
+export const handleNumberCardChange = (event) => {
   event.target.value = event.target.value.replace(/[^0-9]/g, '');
-  if (event.target.value.length <= 16) {
-    setInputValue(event.target.value);
-  }
+
 };
 
-export const handleNumberCvcChange = (event, setInputValue) => {
+export const handleNumberCvcChange = (event) => {
   event.target.value = event.target.value.replace(/[^0-9]/g, '');
-  if (event.target.value.length <= 3) {
-    setInputValue(event.target.value);
-  }
 };
 
